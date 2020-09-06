967d2b9c950531a66290b7a6b42d37f8ed175792
user <wantavielala@gmail.com>
2020-09-06 13:23:12 +0300

    Fix deprecated interpolation-only expressions

--- terraform.tf
+++ terraform.tf
@@ -4,43 +4,43 @@ variable "do_image" {}
 variable "do_size" {}
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 # Create a new SSH key
 resource "digitalocean_ssh_key" "ins_default" {
   name       = "InSales testcase"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 # create two demo droplets
 resource "digitalocean_droplet" "mongo-test1" {
   name     = "mongo-test1"
-  image    = "${var.do_image}"
-  region   = "${var.do_region}"
-  size     = "${var.do_size}"
+  image    = var.do_image
+  region   = var.do_region
+  size     = var.do_size
   ssh_keys = ["${digitalocean_ssh_key.ins_default.fingerprint}"]
 }
 
 resource "digitalocean_droplet" "mongo-test2" {
   name     = "mongo-test2"
-  image    = "${var.do_image}"
-  region   = "${var.do_region}"
-  size     = "${var.do_size}"
+  image    = var.do_image
+  region   = var.do_region
+  size     = var.do_size
   ssh_keys = ["${digitalocean_ssh_key.ins_default.fingerprint}"]
 }
 
 resource "digitalocean_droplet" "mongo-test3" {
   name     = "mongo-test3"
-  image    = "${var.do_image}"
-  region   = "${var.do_region}"
-  size     = "${var.do_size}"
+  image    = var.do_image
+  region   = var.do_region
+  size     = var.do_size
   ssh_keys = ["${digitalocean_ssh_key.ins_default.fingerprint}"]
 }
 
 # create an ansible inventory file
 resource "null_resource" "ansible-provision" {
-  depends_on = ["digitalocean_droplet.mongo-test1", "digitalocean_droplet.mongo-test2", "digitalocean_droplet.mongo-test3"]
+  depends_on = [digitalocean_droplet.mongo-test1, digitalocean_droplet.mongo-test2, digitalocean_droplet.mongo-test3]
 
   provisioner "local-exec" {
     command = "echo [mongo] > hosts"
